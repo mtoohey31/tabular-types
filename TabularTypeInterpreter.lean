769bc44cdd5f0402ef0b2af91075b0132beac19b
Matthew Toohey <contact@mtoohey.com>
2024-08-29 00:28:37 -0400

    initial commit

---
+++ TabularTypeInterpreter.lean
@@ -0,0 +1,1 @@
+import TabularTypeInterpreter.Basic
---
+++ lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+require lott from "vendor/lott"
+
+package «tabular-type-interpreter»
+
+lean_lib TabularTypeInterpreter
