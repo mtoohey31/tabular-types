e9b1dd9719f51ec7e154f75d722b3b3384671c31
Matthew Toohey <contact@mtoohey.com>
2024-09-17 11:26:25 -0400

    F⊗⊕ω types

--- TabularTypeInterpreter/F⊗⊕ω.lean
+++ TabularTypeInterpreter/F⊗⊕ω.lean
@@ -7,4 +7,18 @@ nonterminal Kind, K :=
   | K₁ " ↦ " K₂ : arr
   | "L " K      : list
 
+metavar TypeVar, a
+
+nonterminal Type', A, B :=
+  | a                      : var
+  | "λ " a " : " K ". " A  : lam
+  | A B                    : app
+  | "∀ " a " : " K ". " A  : forall'
+  | A " → " B              : arr
+  | "{" sepBy(A, ", ") "}" : list
+  | A " ⟦" B "⟧"           : listApp
+  | "⊕ " A                 : sum
+  | "⊗ " A                 : prod
+  | "(" A ")"              : paren (desugar := return A)
+
 end TabularTypeInterpreter.«F⊗⊕ω»
