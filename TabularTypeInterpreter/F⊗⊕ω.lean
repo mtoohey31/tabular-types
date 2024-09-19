14daa9445655fa504dff6c5805a878185bcd0cdb
Matthew Toohey <contact@mtoohey.com>
2024-09-18 23:08:28 -0400

    F⊗⊕ω type equivalence and typing

--- TabularTypeInterpreter/F⊗⊕ω.lean
+++ TabularTypeInterpreter/F⊗⊕ω.lean
@@ -1,4 +1,6 @@
 import Lott
+import Lott.Data.Range
+import Lott.DSL.Elab.JudgementComprehension
 import Lott.DSL.Elab.Nat
 
 namespace TabularTypeInterpreter.«F⊗⊕ω»
@@ -21,6 +23,8 @@ nonterminal Type', A, B :=
   | "⊗ " A                 : prod
   | "⊕ " A                 : sum
   | "(" A ")"              : paren (desugar := return A)
+  -- TODO: Replace this with a custom elaboration to a substitution function.
+  | A " [" a " ↦ " B "]"   : subst
 
 metavar TermVar, x
 
@@ -34,8 +38,11 @@ nonterminal Term, E, F :=
   | "π " n E                         : prodElim
   | "ι " n E                         : sumIntro
   | "case " E "{" sepBy(F, ", ") "}" : sumElim
+  -- TODO: Replace these with custom elaborations to a substitution functions.
+  | E " [" a " ↦ " B "]"             : tySubst
+  | E " [" x " ↦ " F "]"             : tmSubst
 
-nonterminal Environemnt, Δ :=
+nonterminal Environment, Δ :=
   | "ε"              : empty
   | Δ ", " x " : " A : termExt
   | Δ ", " a " : " K : typeExt
@@ -86,9 +93,9 @@ a : K ∈ Δ
 ───────────── arr
 Δ ⊢ A → B : *
 
-</ Δ ⊢ As@i : K // i ∈ [0:As.length]ᶠ />
-────────────────────────────────────────────── list
-Δ ⊢ { </ As@i // i ∈ [0:As.length]ᶠ /> } : L K
+</ Δ ⊢ A@i : K // i ∈ [:n] />
+─────────────────────────────────── list
+Δ ⊢ { </ A@i // i ∈ [:n] /> } : L K
 
 Δ ⊢ A : K₁ ↦ K
 Δ ⊢ B : L K₁
@@ -102,5 +109,136 @@ a : K ∈ Δ
 Δ ⊢ A : L *
 ─────────── sum
 Δ ⊢ ⊕ A : *
+
+judgement_syntax x " ≠ " x' : TermVarNe
+
+def TermVarNe := Ne (α := TermVar)
+
+judgement_syntax x " : " A " ∈ " Δ : TermVarInEnvironment
+
+judgement TermVarInEnvironment :=
+
+──────────────── head
+x : A ∈ Δ, x : A
+
+x : A ∈ Δ
+x ≠ x'
+───────────────── termVarExt
+x : A ∈ Δ, x' : B
+
+x : A ∈ Δ
+──────────────── typeVarExt
+x : A ∈ Δ, a : K
+
+judgement_syntax Δ " ⊢ " A " ≡ " B : TypeEquivalence
+
+judgement TypeEquivalence :=
+
+───────── refl
+Δ ⊢ A ≡ A
+
+Δ ⊢ A ≡ B
+───────── symm
+Δ ⊢ B ≡ A
+
+Δ ⊢ B : K
+────────────────────────────── lamApp
+Δ ⊢ (λ a : K. A) B ≡ A [a ↦ B]
+
+</ Δ ⊢ B@i : K // i ∈ [:n] />
+──────────────────────────────────────────────────────────────────────────────── lamListApp
+Δ ⊢ (λ a : K. A) ⟦{ </ B@i // i ∈ [:n] /> }⟧ ≡ { </ A [a ↦ B@i] // i ∈ [:n] /> }
+
+Δ, a : K ⊢ A ≡ B
+─────────────────────────── lam
+Δ ⊢ λ a : K. A ≡ λ a : K. B
+
+Δ ⊢ A₁ ≡ A₂
+Δ ⊢ B₁ ≡ B₂
+───────────────── app
+Δ ⊢ A₁ B₁ ≡ A₂ B₂
+
+Δ, a : K ⊢ A ≡ B
+─────────────────────────── scheme
+Δ ⊢ ∀ a : K. A ≡ ∀ a : K. B
+
+Δ ⊢ A₁ ≡ A₂
+Δ ⊢ B₁ ≡ B₂
+───────────────────── arr
+Δ ⊢ A₁ → B₁ ≡ A₂ → B₂
+
+</ Δ ⊢ A@i ≡ B@i // i ∈ [:n] />
+───────────────────────────────────────────────────────── list
+Δ ⊢ { </ A@i // i ∈ [:n] /> } ≡ { </ B@i // i ∈ [:n] /> }
+
+Δ ⊢ A₁ ≡ A₂
+Δ ⊢ B₁ ≡ B₂
+───────────────────── listApp
+Δ ⊢ A₁ ⟦B₁⟧ ≡ A₂ ⟦B₂⟧
+
+Δ ⊢ A ≡ B
+───────────── prod
+Δ ⊢ ⊗ A ≡ ⊗ B
+
+Δ ⊢ A ≡ B
+───────────── sum
+Δ ⊢ ⊕ A ≡ ⊕ B
+
+───────────── never
+Δ ⊢ ⊕ { } ≡ A
+
+judgement_syntax n " ∈ " "[" n_start ":" n_stop "]" : NatInRange
+
+def NatInRange (n start stop : Nat) := n ∈ [start:stop]
+
+judgement_syntax Δ " ⊢ " E " : " A : Typing
+
+judgement Typing :=
+
+x : A ∈ Δ
+───────── var
+Δ ⊢ x : A
+
+Δ, x : A ⊢ E : B
+────────────────────── lam
+Δ ⊢ λ x : A. E : A → B
+
+Δ ⊢ E : A → B
+Δ ⊢ F : A
+─────────────── app
+Δ ⊢ E F : A → B
+
+Δ, a : K ⊢ E : A
+─────────────────────────── typeAbs
+Δ ⊢ Λ a : K. E : ∀ a : K. A
+
+Δ ⊢ E : ∀ a : K. A
+Δ ⊢ B : K
+───────────────────── typeApp
+Δ ⊢ E [B] : A [a ↦ B]
+
+</ Δ ⊢ E@i : A@i // i ∈ [:n] />
+─────────────────────────────────────────────────────────── prodIntro
+Δ ⊢ ( </ E@i // i ∈ [:n] /> ) : ⊗ { </ A@i // i ∈ [:n] /> }
+
+Δ ⊢ E : ⊗ { </ A@i // i ∈ [:n'] /> }
+n ∈ [0:n']
+──────────────────────────────────── prodElim
+Δ ⊢ π n E : A@n
+
+n ∈ [0:n']
+Δ ⊢ E : A@n
+──────────────────────────────────────── sumIntro
+Δ ⊢ ι n E : ⊕ { </ A@i // i ∈ [:n'] /> }
+
+Δ ⊢ E : ⊕ { </ A@i // i ∈ [:n] /> }
+</ Δ ⊢ F@i : A@i → B // i ∈ [:n] />
+──────────────────────────────────────── sumElim
+Δ ⊢ case E { </ F@i // i ∈ [:n] /> } : B
+
+Δ ⊢ E : A
+Δ ⊢ A ≡ B
+───────── equiv
+Δ ⊢ E : B
 
 end TabularTypeInterpreter.«F⊗⊕ω»
