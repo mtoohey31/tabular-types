d16dc04156588f39b4aa766e5de8a853551a59c2
Matthew Toohey <contact@mtoohey.com>
2024-10-06 00:34:49 -0400

    F⊗⊕ω progress (minus Value.eq_* lemmas)

--- TabularTypeInterpreter/F⊗⊕ω.lean
+++ TabularTypeInterpreter/F⊗⊕ω.lean
@@ -1,4 +1,5 @@
 import Lott
+import Lott.Data.List
 import Lott.Data.Range
 import Lott.DSL.Elab.JudgementComprehension
 import Lott.DSL.Elab.Nat
@@ -206,7 +207,7 @@ x : A ∈ Δ
 Δ ⊢ E F : A → B
 
 Δ, a : K ⊢ E : A
-─────────────────────────── typeAbs
+─────────────────────────── typeLam
 Δ ⊢ Λ a : K. E : ∀ a : K. A
 
 Δ ⊢ E : ∀ a : K. A
@@ -263,12 +264,16 @@ E -> E'
 ─────────────── typeApp
 E [A] -> E' [A]
 
-───────────────────────────── typeAbsApp
+───────────────────────────── typeLamApp
 ⦅Λ a : K. E⦆ [A] -> E [A / a]
 
 E -> E'
 ─────────────────────────────────────────────────────────────────────────────────────────────────────────── prodIntro
 ( </ V@i // i ∈ [:n] />, E, </ F@j // j ∈ [:m] /> ) -> ( </ V@i // i ∈ [:n] />, E', </ F@j // j ∈ [:m] /> )
+
+E -> E'
+───────────────────────────────────── prodElim
+π n E -> π n E'
 
 n ∈ [0:n']
 ───────────────────────────────────── prodElimIntro
@@ -290,7 +295,147 @@ n ∈ [0:n']
 ──────────────────────────────────────────── sumElimIntro
 case V { </ V'@i // i ∈ [:n'] /> } -> V'@n V
 
-theorem progress : [[ε ⊢ E : A]] → [[E -> E']] ∨ E.IsValue := sorry
+private
+theorem Value.eq_lam_of_ty_arr (VtyAarrB : [[ε ⊢ V : A → B]])
+  : ∃ x A' E, V.1 = [[λ x : A'. E]] := sorry
+
+private
+theorem Value.eq_typeApp_of_ty_forall (Vty : [[ε ⊢ V : ∀ a : K. A]])
+  : ∃ a K E, V.1 = [[Λ a : K. E]] := sorry
+
+private
+theorem Value.eq_prodIntro_of_ty_prod (Vty : [[ε ⊢ V : ⊗ { </ A@i // i ∈ [0:n] /> }]])
+  : ∃ V' : Nat → Value, V.1 = [[( </ V'@i // i ∈ [0:n] /> )]] := sorry
+
+private
+theorem Value.eq_sumIntro_of_ty_sum (Vty : [[ε ⊢ V : ⊕ { </ A@i // i ∈ [0:n'] /> }]])
+  : ∃ n ∈ [0:n'], ∃ E, V.1 = [[ι n E]] := sorry
+
+open Std
+
+private
+theorem _root_.Membership.mem.inc {r : Range} (h : i ∈ r) : i ∈ { r with stop := r.stop + 1 } :=
+  ⟨h.lower, Nat.lt_add_right _ h.upper⟩
+
+private
+instance : Inhabited Value where
+  default := ⟨.prodIntro [], .prodIntro fun _ mem => by cases mem⟩
+
+private
+theorem progress.fold {E : Nat → Term} (EtyA : ∀ i ∈ [0:n], [[ε ⊢ E@i : A@i]])
+  (h : ∀ i ∈ [0:n], (∃ E', [[E@i -> E']]) ∨ (E i).IsValue) :
+  (∃ i ∈ [0:n], (∀ j ∈ [0:i], (E j).IsValue) ∧ ∃ E', [[E@i -> E']]) ∨
+    (∀ i ∈ [0:n], (E i).IsValue) := by
+  induction n
+  · case zero =>
+    right
+    intro _ ⟨_, lt_zero⟩
+    contradiction
+  · case succ j ih' => match ih' (fun j mem => EtyA j mem.inc) (fun j mem => h j mem.inc) with
+    | .inl ⟨i, mem, ⟨E'', toE''⟩⟩ => exact .inl ⟨i, mem.inc, ⟨E'', toE''⟩⟩
+    | .inr IsValue =>
+      have jmem : j ∈ [0:j + 1] := ⟨Nat.zero_le _, Nat.lt_succ_self _⟩
+      match h j jmem with
+      | .inl ⟨E'', toE''⟩ => exact .inl ⟨j, jmem, ⟨IsValue, ⟨E'', toE''⟩⟩⟩
+      | .inr jIsValue =>
+        right
+        intro i mem
+        by_cases i = j
+        · case pos h =>
+          rw [h]
+          exact jIsValue
+        · case neg h =>
+          exact IsValue i ⟨Nat.zero_le _, Nat.lt_of_le_of_ne (Nat.le_of_lt_succ mem.upper) h⟩
+
+private
+theorem progress.sandwich {f : Nat → α} (h : i < n) : (List.map (fun j => [f j]) [0:n]).join =
+  (List.map (fun j => [f j]) [0:i]).join ++
+    f i :: (List.map (fun j => [f (j + (i + 1))]) [(i + 1) - (i + 1):n - (i + 1)]).join := by
+  simp only [List.map_singleton_join]
+  congr
+  rw [← List.singleton_append, Range.map_shift (Nat.le_refl _)]
+  have : [f i] = List.map (fun j => f j) [i:i + 1] := by
+    rw [Range.toList, if_neg Nat.one_ne_zero, if_pos (Nat.lt_succ_self _), Range.toList,
+        if_neg Nat.one_ne_zero, if_neg (Nat.not_lt_of_le (Nat.le_refl _)), List.map, List.map]
+  rw [this]
+  rw [Range.map_append (Nat.le_succ _) (Nat.succ_le_of_lt h),
+      Range.map_append (Nat.zero_le _) (Nat.le_of_lt h)]
+
+theorem progress (EtyA : [[ε ⊢ E : A]]) : (∃ E', [[E -> E']]) ∨ E.IsValue := by
+  generalize Δeq : Environment.empty = Δ at EtyA
+  induction EtyA <;> cases Δeq
+  · case var xinε => cases xinε
+  · case lam => exact .inr .lam
+  · case app E' A' B F E'tyA'arrB _ ih₁ ih₂ => match ih₁ rfl with
+    | .inl ⟨E'', E'toE''⟩ => exact .inl <| .intro _ <| .appL E'toE''
+    | .inr E'IsValue => match ih₂ rfl with
+      | .inl ⟨F', FtoF'⟩ => exact .inl <| .intro _ <| .appR (V := ⟨E', E'IsValue⟩) FtoF'
+      | .inr FIsValue =>
+        let VE' : Value := ⟨E', E'IsValue⟩
+        have : E' = VE'.1 := rfl
+        have ⟨_, _, _, VE'eq⟩ := VE'.eq_lam_of_ty_arr E'tyA'arrB
+        rw [this, VE'eq]
+        exact .inl <| .intro _ <| .lamApp (V := ⟨F, FIsValue⟩)
+  · case typeLam => exact .inr .typeLam
+  · case typeApp E' a K _ A' E'ty _ ih => match ih rfl with
+    | .inl ⟨E'', E'toE''⟩ => exact .inl <| .intro _ <| .typeApp E'toE''
+    | .inr E'IsValue =>
+      let V : Value := ⟨E', E'IsValue⟩
+      have : E' = V.1 := rfl
+      have ⟨_, _, _, Veq⟩ := V.eq_typeApp_of_ty_forall E'ty
+      rw [this, Veq]
+      exact .inl <| .intro _ <| .typeLamApp
+  · case prodIntro n E' A E'ty ih => match progress.fold E'ty (fun i mem => ih i mem rfl) with
+    | .inl ⟨i, ⟨_, iltn⟩, IsValue, E'', toE''⟩ =>
+      let V j : Value := if h' : j < i then ⟨E' j, IsValue j ⟨Nat.zero_le _, h'⟩⟩ else default
+      dsimp only [Coe.coe]
+      rw [progress.sandwich iltn, Range.map_eq_of_eq_of_mem (fun j jmem => by
+          show [E' j] = [(V j).val]
+          dsimp only [V]
+          rw [dif_pos jmem.upper]
+      ), Nat.sub_self]
+      exact .inl <| .intro _ <| .prodIntro toE''
+    | .inr IsValue =>
+      exact .inr <| .prodIntro fun E Emem => by
+        rw [List.map_singleton_join] at Emem
+        have ⟨i, imem, Eeq⟩ := Range.mem_of_mem_map Emem
+        rw [Eeq]
+        exact IsValue i imem
+  · case prodElim E' n _ i mem E'ty ih => match ih rfl with
+    | .inl ⟨E'', E'toE''⟩ => exact .inl <| .intro _ <| .prodElim E'toE''
+    | .inr E'IsValue =>
+      let V : Value := ⟨E', E'IsValue⟩
+      have : E' = V.1 := rfl
+      have ⟨_, Veq⟩ := V.eq_prodIntro_of_ty_prod E'ty
+      rw [this, Veq]
+      exact .inl <| .intro _ <| .prodElimIntro mem
+  · case sumIntro ih => match ih rfl with
+    | .inl ⟨E', toE'⟩ => exact .inl <| .intro _ <| .sumIntro toE'
+    | .inr E'IsValue => exact .inr <| .sumIntro E'IsValue
+  · case sumElim E' n As F _ E'ty Fty ih₁ ih₂ => match ih₁ rfl with
+    | .inl ⟨E'', E'toE''⟩ => exact .inl <| .intro _ <| .sumElimL E'toE''
+    | .inr E'IsValue =>
+      let VE' : Value := ⟨E', E'IsValue⟩
+      have ⟨_, mem, _⟩ := VE'.eq_sumIntro_of_ty_sum E'ty
+      match progress.fold Fty (fun i mem => ih₂ i mem rfl) with
+      | .inl ⟨j, ⟨_, jltn⟩, IsValue, F', toF'⟩ =>
+        let VF k : Value := if h' : k < j then ⟨F k, IsValue k ⟨Nat.zero_le _, h'⟩⟩ else default
+        dsimp only [Coe.coe]
+        rw [progress.sandwich jltn, Range.map_eq_of_eq_of_mem (fun j jmem => by
+          show [F j] = [(VF j).val]
+          dsimp only [VF]
+          rw [dif_pos jmem.upper]
+        ), Nat.sub_self]
+        exact .inl <| .intro _ <| .sumElimR (V := VE') toF'
+      | .inr FIsValue =>
+        let VF j : Value := if h : j < n then ⟨F j, FIsValue j ⟨Nat.zero_le _, h⟩⟩ else default
+        rw [List.map_singleton_join, Range.map_eq_of_eq_of_mem' (fun i mem => by
+          show F i = (VF i).val
+          dsimp only [VF]
+          rw [dif_pos mem.upper]
+        ), ← List.map_singleton_join]
+        exact .inl <| .intro _ <| .sumElimIntro (V := VE') mem
+  · case equiv ih => exact ih rfl
 
 theorem preservation : [[ε ⊢ E : A]] → [[E -> E']] → [[ε ⊢ E' : A]] := sorry
 
