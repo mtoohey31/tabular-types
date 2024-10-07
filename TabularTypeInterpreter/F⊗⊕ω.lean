e3c38fc25c1182f0e7ae63d655d6a6e4f1f406ed
Matthew Toohey <contact@mtoohey.com>
2024-10-07 14:48:35 -0400

    F⊗⊕ω TypeEquivalence improvements and theorems

--- TabularTypeInterpreter/F⊗⊕ω.lean
+++ TabularTypeInterpreter/F⊗⊕ω.lean
@@ -135,17 +135,21 @@ judgement TypeEquivalence :=
 ───────── refl
 Δ ⊢ A ≡ A
 
-Δ ⊢ A ≡ B
-───────── symm
-Δ ⊢ B ≡ A
-
 Δ ⊢ B : K
-────────────────────────────── lamApp
+────────────────────────────── lamAppL
 Δ ⊢ (λ a : K. A) B ≡ A [B / a]
 
+Δ ⊢ B : K
+────────────────────────────── lamAppR
+Δ ⊢ A [B / a] ≡ (λ a : K. A) B
+
 </ Δ ⊢ B@i : K // i ∈ [:n] />
-──────────────────────────────────────────────────────────────────────────────── lamListApp
+──────────────────────────────────────────────────────────────────────────────── lamListAppL
 Δ ⊢ (λ a : K. A) ⟦{ </ B@i // i ∈ [:n] /> }⟧ ≡ { </ A [B@i / a] // i ∈ [:n] /> }
+
+</ Δ ⊢ B@i : K // i ∈ [:n] />
+──────────────────────────────────────────────────────────────────────────────── lamListAppR
+Δ ⊢ { </ A [B@i / a] // i ∈ [:n] /> } ≡ (λ a : K. A) ⟦{ </ B@i // i ∈ [:n] /> }⟧
 
 Δ, a : K ⊢ A ≡ B
 ─────────────────────────── lam
@@ -182,8 +186,62 @@ judgement TypeEquivalence :=
 ───────────── sum
 Δ ⊢ ⊕ A ≡ ⊕ B
 
-───────────── never
-Δ ⊢ ⊕ { } ≡ A
+private
+def TypeEquivalence.symm : [[Δ ⊢ A ≡ B]] → [[Δ ⊢ B ≡ A]]
+  | .refl => .refl
+  | .lamAppL h => .lamAppR h
+  | .lamAppR h => .lamAppL h
+  | .lamListAppL h => .lamListAppR h
+  | .lamListAppR h => .lamListAppL h
+  | .lam h => .lam h.symm
+  | .app h₁ h₂ => .app h₁.symm h₂.symm
+  | .scheme h => .scheme h.symm
+  | .arr h₁ h₂ => .arr h₁.symm h₂.symm
+  | .list h => .list (fun i mem => (h i mem).symm)
+  | .listApp h₁ h₂ => .listApp h₁.symm h₂.symm
+  | .prod h => .prod h.symm
+  | .sum h => .sum h.symm
+
+judgement_syntax Δ " ⊢ " A " ≢ " B : TypeInequivalence
+
+def TypeInequivalence Δ A B := ¬[[Δ ⊢ A ≡ B]]
+
+namespace TypeInequivalence
+
+private
+def symm : [[Δ ⊢ A ≢ B]] → [[Δ ⊢ B ≢ A]] := (· ·.symm)
+
+private
+theorem arr_forall : [[Δ ⊢ A → B ≢ ∀ a : K. A']] := fun equ => by
+  generalize A₁eq : [[(A → B)]] = A₁, A₂eq : [[∀ a : K. A']] = A₂ at equ
+  induction equ <;> ((try contradiction); try cases A₁eq; contradiction)
+
+private
+theorem arr_prod : [[Δ ⊢ A → B ≢ ⊗ A']] := fun equ => by
+  generalize A₁eq : [[(A → B)]] = A₁, A₂eq : [[⊗ A']] = A₂ at equ
+  induction equ <;> ((try contradiction); try cases A₁eq; contradiction)
+
+private
+theorem arr_sum : [[Δ ⊢ A → B ≢ ⊕ A']] := fun equ => by
+  generalize A₁eq : [[(A → B)]] = A₁, A₂eq : [[⊕ A']] = A₂ at equ
+  induction equ <;> ((try contradiction); try cases A₁eq; contradiction)
+
+private
+theorem forall_prod : [[Δ ⊢ ∀ a : K. A ≢ ⊗ B]] := fun equ => by
+  generalize A₁eq : [[∀ a : K. A]] = A₁, A₂eq : [[⊗ B]] = A₂ at equ
+  induction equ <;> ((try contradiction); try cases A₁eq; contradiction)
+
+private
+theorem forall_sum : [[Δ ⊢ ∀ a : K. A ≢ ⊕ B]] := fun equ => by
+  generalize A₁eq : [[∀ a : K. A]] = A₁, A₂eq : [[⊕ B]] = A₂ at equ
+  induction equ <;> ((try contradiction); try cases A₁eq; contradiction)
+
+private
+theorem prod_sum : [[Δ ⊢ ⊗ A ≢ ⊕ B]] := fun equ => by
+  generalize A₁eq : [[⊗ A]] = A₁, A₂eq : [[⊕ B]] = A₂ at equ
+  induction equ <;> ((try contradiction); try cases A₁eq; contradiction)
+
+end TypeInequivalence
 
 judgement_syntax n " ∈ " "[" n_start ":" n_stop "]" : NatInRange
 
