9452445babd90761f4cdfdf70d0f0a7c1f4a7da3
LightQuantum <self@lightquantum.me>
2025-02-25 19:58:45 -0500

    Term.TypeVarLocallyClosed_of

--- TabularTypeInterpreter/F⊗⊕ω/Lemmas/Term.lean
+++ TabularTypeInterpreter/F⊗⊕ω/Lemmas/Term.lean
@@ -124,6 +124,77 @@ theorem sum_id (Δwf : [[⊢ Δ]]) (Aki : [[Δ ⊢ A : L K]])
       rw [Aki.TypeVarLocallyClosed_of.TypeVar_open_eq]
       exact Aki.weakening (Δ' := .typeExt .empty ..) (Δ'' := .empty) <| Δwf.typeVarExt anin
 
+theorem WellFormedness_of (EtyA : [[Δ ⊢ E : A]]) : [[ ⊢ Δ ]] := by
+  induction EtyA <;> try simp_all; done
+  . case lam Δ T E A I EtyA ih =>
+    have ⟨x, notIn⟩ := I.exists_fresh
+    specialize ih x notIn
+    cases ih; assumption
+  . case typeLam Δ K E A I EtyA ih =>
+    have ⟨a, notIn⟩ := I.exists_fresh
+    specialize ih a notIn
+    cases ih; assumption
+
+-- TODO naming
+theorem Δext_TypeVarLocallyClosed_of' (EtyA : [[Δ, x: T, Δ' ⊢ E : A]]) : T.TypeVarLocallyClosed := by
+  have wf := EtyA.WellFormedness_of; clear EtyA
+  have fresh := wf.append_termVar_fresh_r x (by simp [Environment.termVarDom])
+  induction Δ'
+  . case empty =>
+    cases wf; case termVarExt wf notIn Tki => exact Tki.TypeVarLocallyClosed_of
+  . case typeExt Δ' x' K ih => exact ih (by cases wf; assumption) fresh
+  . case termExt Δ' x' T' ih =>
+    exact ih (by cases wf; assumption) (by intro contra; exact fresh (by simp_all [Environment.termVarDom]))
+
+theorem Δext_TypeVarLocallyClosed_of (EtyA : [[Δ, x: T ⊢ E : A]]) : T.TypeVarLocallyClosed :=
+  EtyA.Δext_TypeVarLocallyClosed_of' (Δ' := .empty)
+
+theorem TypeVarLocallyClosed_of (EtyA : [[Δ ⊢ E : A]]) : A.TypeVarLocallyClosed 0 := by
+  induction EtyA
+  . case var Δ x A wf In =>
+    induction In <;> (try cases wf; simp_all)
+    match wf with | .termVarExt _ _ k => exact k.TypeVarLocallyClosed_of
+  . case lam Δ T E A I EtyA ih =>
+    let ⟨x, xnin⟩ := I.exists_fresh
+    constructor
+    specialize EtyA x xnin
+    . exact EtyA.Δext_TypeVarLocallyClosed_of
+    . exact ih x xnin
+  . case app _ _ _ _ _ _ _ ih1 ih2 => cases ih1; assumption
+  . case typeLam Δ K E A I EtyA ih =>
+    let ⟨a, anin⟩ := (I ++ A.freeTypeVars).exists_fresh
+    constructor
+    specialize ih a (by simp_all)
+    have := ih.TypeVar_close_inc (a := a)
+    rw [Type.TypeVar_close_TypeVar_open_eq_of_not_mem_freeTypeVars (by simp_all)] at this
+    exact this
+  . case typeApp Δ E K A B EtyA BkiK ih => cases ih; case «forall» Alc =>
+    exact Alc.Type_open_dec BkiK.TypeVarLocallyClosed_of
+  . case prodIntro n Δ E A EtyA ih =>
+    constructor
+    constructor
+    intro A_ In
+    simp [List.map_singleton_flatten] at In
+    have ⟨i, In, A_eq⟩ := In
+    subst A_eq
+    exact ih i (by simp_all [Std.Range.mem_of_mem_toList])
+  . case prodElim Δ E n As i EtyAs In ih =>
+    simp [List.map_singleton_flatten] at ih
+    cases ih; case prod ih => cases ih; case list ih =>
+    exact ih (As i) (Std.Range.mem_map_of_mem In)
+  . case sumIntro i n Δ E As In EtyA Aki ih =>
+    constructor
+    constructor
+    intro A AInAs
+    simp [List.map_singleton_flatten] at AInAs
+    obtain ⟨j, AInAs, A_eq⟩ := AInAs
+    subst A_eq
+    exact Aki j (by simp_all [Std.Range.mem_of_mem_toList]) |>.TypeVarLocallyClosed_of
+  . case sumElim Δ E n As Fs B EtyA FtyAB Bki ih1 ih2 =>
+    exact Bki.TypeVarLocallyClosed_of
+  . case equiv Δ E A B EtyA eqAB ih => sorry -- TODO by equiv -> equiv closure of pred -> equiv pred preserves lc
+
+
 theorem TermVarLocallyClosed_of (EtyA : [[Δ ⊢ E : A]]) : E.TermVarLocallyClosed 0 := by
   induction EtyA with
   | var _ _ => exact .var_free
@@ -135,15 +206,15 @@ theorem TermVarLocallyClosed_of (EtyA : [[Δ ⊢ E : A]]) : E.TermVarLocallyClos
     let ⟨x, xnin⟩ := I.exists_fresh
     exact .typeLam <| ih x xnin |>.TypeVar_open_drop
   | typeApp _ _ ih => exact .typeApp ih
-  | prodIntro _ ih =>
+  | prodIntro _ _ ih =>
     exact .prodIntro fun E mem => by
       rw [List.map_singleton_flatten] at mem
       let ⟨i, mem', eq⟩ := Std.Range.mem_of_mem_map mem
       cases eq
       exact ih i mem'
   | prodElim _ _ ih => exact .prodElim ih
-  | sumIntro _ _ ih => exact .sumIntro ih
-  | sumElim _ _ ih₀ ih₁ =>
+  | sumIntro _ _ _ ih => exact .sumIntro ih
+  | sumElim _ _ _ ih₀ ih₁ =>
     exact .sumElim ih₀ fun i mem => by
       rw [List.map_singleton_flatten] at mem
       let ⟨i, mem', eq⟩ := Std.Range.mem_of_mem_map mem
